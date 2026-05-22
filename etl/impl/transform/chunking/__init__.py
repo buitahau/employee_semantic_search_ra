6794db5412c5a6e8b85f53c63449ae03284107fb
HauBui <buitahau@gmail.com>
2026-05-22 10:26:42 +0700

    feat: T3.5 chunking - Create employee embedding data

--- etl/impl/transform/chunking/__init__.py
+++ etl/impl/transform/chunking/__init__.py
@@ -3,6 +3,7 @@ from etl.impl.transform.chunking.tokenizer import _fill_token_counts
 from etl.impl.transform.chunking.splitter import _fill_sentences
 from etl.impl.transform.chunking.accumulator import _fill_windows
 from etl.impl.transform.chunking.builder import _fill_chunks
+from etl.impl.transform.chunking.embedding import embed_chunks
 
 
 def chunking(data: EmployeeData) -> EmployeeData:
@@ -10,4 +11,5 @@ def chunking(data: EmployeeData) -> EmployeeData:
     data = _fill_sentences(data)
     data = _fill_windows(data)
     data = _fill_chunks(data)
+    data = embed_chunks(data)
     return data
