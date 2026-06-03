bc064454bb893a7d0861faf2dc1b97de511b7111
HauBui <buitahau@gmail.com>
2026-06-03 14:18:19 +0700

    feat: create search api #Q1.1

--- api/routers/search.py
+++ api/routers/search.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Body
+from fastapi.responses import JSONResponse
+
+from query.types import QueryRequest
 
 router = APIRouter()
 
 
 @router.get("/search")
-async def search():
-    raise NotImplementedError
+async def search(request: Annotated[QueryRequest, Body()]):
+    return JSONResponse(status_code=200, content={"message": "ok"})
