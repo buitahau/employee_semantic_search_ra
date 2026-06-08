ed4c3c9043b237168e2b2ae9437ed03d43050658
NguyenThiHoa <hnguyen@openwt.com>
2026-06-08 09:44:45 +0700

    feat: fix docker local dev setup

--- docker/init/01-init-databases.sql
+++ docker/init/01-init-databases.sql
@@ -1,7 +1,10 @@
+-- Create the vector DB (runs while connected to owt_employee, the POSTGRES_DB default)
 CREATE DATABASE owt_vector;
 
--- connect owt_vector
+-- Switch to owt_vector and install the pgvector extension
+\c owt_vector
 CREATE EXTENSION IF NOT EXISTS vector;
 
--- connect owt_employee
+-- Switch back to owt_employee and install the pgvector extension
+\c owt_employee
 CREATE EXTENSION IF NOT EXISTS vector;
